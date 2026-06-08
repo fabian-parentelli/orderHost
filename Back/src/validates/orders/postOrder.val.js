568451c0ec45f521d954cb02ff210dabaa8e63f0
Fabian Parentelli <fabianparentelli007code@gmail.com>
2026-06-08 19:01:24 -0300

    validation in cart

--- Back/src/validates/orders/postOrder.val.js
+++ Back/src/validates/orders/postOrder.val.js
@@ -14,32 +14,6 @@ const postOrder = (body) => {
         throw new ErrorNotFound("El carrito está vacío o no es un formato válido.");
     };
 
-    for (const item of cart) {
-        const itemKeys = Object.keys(item);
-        const allowedKeys = ['pid', 'quantity', 'price'];
-
-        const hasExtraKeys = itemKeys.length !== allowedKeys.length || !itemKeys.every(key => allowedKeys.includes(key));
-
-        if (hasExtraKeys) {
-            throw new ErrorNotFound("Se detectaron propiedades no permitidas dentro de los artículos del carrito.");
-        };
-
-        const { pid, quantity, price } = item;
-
-        if (!pid || !isValidObjectId(pid)) {
-            throw new ErrorNotFound(`El ID de producto '${pid}' no es un ObjectId válido.`);
-        };
-
-        if (!Number.isInteger(quantity) || quantity <= 0) {
-            throw new ErrorNotFound(`La cantidad (${quantity}) debe ser un número entero mayor a 0.`);
-        };
-
-        if (!Number.isInteger(price) || price <= 0) {
-            throw new ErrorNotFound(`El precio (${price}) debe ser un número entero mayor a 0.`);
-        };
-
-    };
-
     return cart;
 };
 
